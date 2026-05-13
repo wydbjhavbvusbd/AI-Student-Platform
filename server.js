8487d3ab265c765ab65a7d060941bb9acc206652
Edward Mosses <Edward Mosses>
2026-05-13 16:45:44 +0800

    Updated deployment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-const db = require('./config/db');
+const db = require('./db');
 const Message = require('./models/Message');
 const authRoutes = require('./routes/auth');
 const multer = require('multer');
